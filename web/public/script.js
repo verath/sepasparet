f8c3d664b28e89135c5d3dccdf8ac996ea626155
Peter Eliasson <warackta@gmail.com>
2022-11-19 19:24:19 +0100

    Update

---
+++ web/public/script.js
@@ -0,0 +1,154 @@
+const PA_SPARET_UPDATE = (function () {
+    'use strict';
+
+    const PA_SPARET_PROXY_URL = "https://app.sepåspåret.se/";
+
+    function plot(graphElem, data) {
+        graphElem.innerHTML = "";
+        // data.highscore
+        // data.me_profile
+        let me_profile = data.me_profile;
+        let highscore = data.highscore;
+        let meUserId = me_profile.userId;
+
+        // userId => playerData
+        let players = new Map();
+        players.set(me_profile.userId, {
+            firstName: me_profile.firstName,
+            lastName: me_profile.lastName,
+            userId: me_profile.userId,
+            username: me_profile.username,
+            profile: {
+                color: me_profile.profile.color,
+                imageUrl: me_profile.profile.imageUrl,
+            }
+        });
+
+        for (let ep in highscore.highscores.byEpisode) {
+            let episodeData = highscore.highscores.byEpisode[ep][0];
+            episodeData.friends.forEach((playerData) => {
+                players.set(playerData.userId, {
+                    firstName: playerData.firstName,
+                    lastName: playerData.lastName,
+                    userId: playerData.userId,
+                    username: playerData.username,
+                    profile: {
+                        color: playerData.profile.color,
+                        imageUrl: playerData.profile.imageUrl,
+                    }
+                });
+            })
+        }
+        let playerIds = [];
+        players.forEach((_v, k) => playerIds.push(k));
+
+        // "userId" => totalScore
+        // For some reason this is not the same as the sum of all episode
+        // scores for some players??
+        let playerTotalScores = new Map(playerIds.map(pId => [pId, 0]));
+        playerTotalScores.set(meUserId, highscore.highscores.forSeason[0].user.points);
+        for (let i = 0; i < highscore.highscores.forSeason[0].friends.length; i++) {
+            let friendData = highscore.highscores.forSeason[0].friends[i];
+            playerTotalScores.set(friendData.userId, friendData.points);
+        }
+
+        // "userId" => [scoreEp1, scoreEp2, ...]
+        let playerScores = new Map(playerIds.map(pId => [pId, []]));
+        let episodes = [];
+        for (let ep in highscore.highscores.byEpisode) {
+            let episodeData = highscore.highscores.byEpisode[ep][0];
+            let meScore = episodeData.user.points || 0;
+            let friendScores = episodeData.friends.map((friendData) =>
+                [friendData.userId, friendData.points]
+            );
+            if (meScore == 0 && friendScores.length == 0) {
+                continue; // This episode was not played by anyone.
+            }
+            episodes.push(ep);
+            let episodeScores = [[meUserId, meScore]];
+            episodeScores = episodeScores.concat(friendScores);
+            // Add players that did not participate in this episode.
+            let episodeUserIds = episodeScores.map(([id, _]) => id);
+            let missingPlayerIds = playerIds.filter(pId => episodeUserIds.indexOf(pId) === -1);
+            episodeScores = episodeScores.concat(missingPlayerIds.map(pId => [pId, NaN]));
+
+            episodeScores.forEach(([id, score]) => {
+                let s = playerScores.get(id) || [];
+                s.push(score);
+                playerScores.set(id, s);
+            });
+        }
+
+        // Sort playerScores by highest total score
+        playerScores = new Map([...playerScores].sort((a, b) => {
+            let aTotalScore = playerTotalScores.get(a[0]) || 0;
+            let bTotalScore = playerTotalScores.get(b[0]) || 0;
+            return bTotalScore - aTotalScore;
+        }));
+
+        let plotData = [];
+        for (let entry of playerScores) {
+            let [userId, episodeScores] = entry;
+            let player = players.get(userId);
+            let totalScore = playerTotalScores.get(userId);
+            plotData.push({
+                x: episodes,
+                y: episodeScores,
+                type: 'line',
+                // SPACES??? YES I DONT KNOW
+                name: `${player.firstName} ${player.lastName[0]} [${totalScore}]                      `,
+            })
+        }
+
+        let layout = {
+            autosize: true,
+            plot_bgcolor: "#2f4f4f",
+            paper_bgcolor: "#2f4f4f",
+            font: {
+                family: 'Roboto',
+                color: '#DDB'
+            },
+            titlefont: {
+                size: 18,
+            },
+            yaxis: {
+                title: 'Score'
+            },
+            title: "(↑) På Spåret",
+            xaxis: {
+                tickvals: episodes,
+                ticktext: episodes.map(ep => `E${ep}`),
+            }
+        };
+
+        let plot_options = {
+            responsive: true,
+            displayModeBar: true,
+            displaylogo: false,
+            modeBarButtonsToRemove: [
+                'toggleSpikelines',
+                'zoom2d',
+                'pan2d',
+                'zoomIn2d',
+                'zoomOut2d',
+                'select2d',
+                'lasso2d',
+                'autoScale2d',
+                'toImage']
+        };
+        Plotly.newPlot(graphElem, plotData, layout, plot_options);
+    }
+
+    function update(graphElem) {
+        return fetch(PA_SPARET_PROXY_URL)
+            .then(r => r.json())
+            .then((data) => {
+                plot(graphElem, data);
+            })
+            .catch(err => console.error("Error fetching på spåret", err));
+    };
+    return update;
+})();
+
+
+PA_SPARET_UPDATE(document.getElementById('graph-container'));
